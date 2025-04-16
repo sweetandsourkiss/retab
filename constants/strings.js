d94024bb8d91c8aea8f0c4ac2d8d7ec65b9219a0
Jay Son <mooul0529@naver.com>
2025-04-16 22:56:12 +0900

    Add const string file

---
+++ constants/strings.js
@@ -0,0 +1,10 @@
+// tags
+export const TAG_DIV = "div";
+export const TAG_ANCHOR = "a";
+
+// classes
+export const GRAPH = "graph";
+export const GRAPH_FLEX_COLUMN = "graph-flex-column";
+export const GRAPH_HOSTNAME = "graph-hostname";
+export const GRAPH_VISIT = "graph-visit";
+export const GRAPH_RATIO = "graph-ratio";
