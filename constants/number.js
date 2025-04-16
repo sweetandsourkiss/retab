199cfe92c1e51f82322d9ab53afd026b3fddcb2e
Jay Son <mooul0529@naver.com>
2025-04-16 22:55:58 +0900

    Add const number file

---
+++ constants/number.js
@@ -0,0 +1,9 @@
+// 일주일
+// const millisecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
+// 한 달
+// const millisecondsPerMonth = 1000 * 60 * 60 * 24 * 7 * 4;
+// 일 년
+// const millisecondsPerYear = 1000 * 60 * 60 * 24 * 7 * 365;
+// 기본값
+export const DEFAULT_TIME = 0;
+export const MAX_RESULT = 100000000;
